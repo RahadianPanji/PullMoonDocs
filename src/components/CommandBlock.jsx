baba0f56eead49aa955879da325a6459c20ad2b7
Panji <rahadianpanjipanji.com>
2021-11-04 18:10:06 +0700

    replace == with ===

--- src/components/CommandBlock.jsx
+++ src/components/CommandBlock.jsx
@@ -10,10 +10,10 @@ const CommandBlock = () => {
                 commandsList.map(now => (
                     <div className="MiniBlock">
                         <h3>{now.title}
-                        {now.title=="NSFW" &&
+                        {now.title === "NSFW" &&
                             <span onClick={() => setShowNSFW(!showNSFW)} style={{color: "#0066cc",cursor:"pointer", fontSize: "60%", marginLeft: 9}}>{showNSFW ? "Hide" : "Show"}</span>
                         }</h3> 
-                        { (now.title != "NSFW" || showNSFW)  &&
+                        { (now.title !== "NSFW" || showNSFW)  &&
                             now.body.map((child) => <CommandChild content={child}/>)
                         }
                     </div>
